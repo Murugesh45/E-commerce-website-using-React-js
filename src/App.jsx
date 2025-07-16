60e15b4fcd4c723e6676c186d728dc574e5d085a
Murugesh45 <murugeshrohit45@gmail.com>
2025-07-16 15:57:00 +0530

    change color

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ function App() {
           <Route path= {"/signup"} element={<Signup />} />
           <Route path= {"/About"} element={<About />} />
         </Routes>
+         
         
       </div>
       
