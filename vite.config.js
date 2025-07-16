8f721e77941cd4dd291121ed06a6adb316fc9175
Murugesh45 <murugeshrohit45@gmail.com>
2025-07-16 16:30:40 +0530

    change path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/E-commerce-website-using-React-js'
+  base: process.env.VITE_BASE_PATH || "/E-commerce-website-using-React-js"
 })
